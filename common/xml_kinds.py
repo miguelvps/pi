894809ca86f4216b40b6d665db8b4b5c77f53d9a
goncalopp <goncalopp@gmail.com>
2011-04-12 10:40:43 +0100

    xml_kinds: added "list". small syntax change to xml_types

--- common/xml_kinds.py
+++ common/xml_kinds.py
@@ -1,14 +1,10 @@
-from flask import Flask
-from flaskext.sqlalchemy import SQLAlchemy
+import sqlalchemy
 import xml_types
 
-app = Flask(__name__)
-db = SQLAlchemy(app)
-
-class birthdate (db.Date):   type = xml_types.DATETIME_TYPE
-class name      (db.String): type = xml_types.STRING_TYPE
-class office    (db.String): type = xml_types.STRING_TYPE
-class contact   (db.String): type = xml_types.STRING_TYPE
-class email     (db.String): type = xml_types.EMAIL_TYPE
-class phone     (db.String): type = xml_types.STRING_TYPE
-class fax       (db.String): type = xml_types.STRING_TYPE
+class birthdate (sqlalchemy.types.Date):   type = xml_types.DATETIME_TYPE
+class name      (sqlalchemy.types.String): type = xml_types.STRING_TYPE
+class office    (sqlalchemy.types.String): type = xml_types.STRING_TYPE
+class contact   (sqlalchemy.types.String): type = xml_types.STRING_TYPE
+class email     (sqlalchemy.types.String): type = xml_types.EMAIL_TYPE
+class phone     (sqlalchemy.types.String): type = xml_types.STRING_TYPE
+class fax       (sqlalchemy.types.String): type = xml_types.STRING_TYPE
