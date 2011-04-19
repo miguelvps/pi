e50e0382bd20f883a5ad76b33fefb3352e819075
Miguel Gomes <mp.gomes89@gmail.com>
2011-04-19 20:15:14 +0100

    vários bugs

--- concierge/app.py
+++ concierge/app.py
@@ -12,6 +12,13 @@ def index():
 def historico():
     return render_template('historico.html')
 
+@app.route('/login/')
+def login():
+    return render_template('login.html')
 
+@app.route('/service/')
+def service():
+    return render_template('service.html')
+	
 if __name__ == '__main__':
     app.run(debug=True)
