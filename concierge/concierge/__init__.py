31cd253d68cfd723f0f5ef31be3e584fc800f135
Miguel Serrano <miguelvps@gmail.com>
2011-04-20 15:42:59 +0100

    Concierge: auth module skeleton

--- concierge/concierge/__init__.py
+++ concierge/concierge/__init__.py
@@ -3,8 +3,13 @@ from flaskext.sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'k,\xa3u\xa7;c\x1c\x9f\xc5"2\x8esF:\xb0F\xa6A?\x9bXC'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///concierge.db'
 db = SQLAlchemy(app)
+
+
+from concierge.auth import auth
+app.register_module(auth, url_prefix='/auth')
 
 
 @app.route('/')
