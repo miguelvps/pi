d27b1b5c1276c8c542ca5e7c1486994249dd8ae5
Miguel Serrano <miguelvps@gmail.com>
2011-04-20 16:50:44 +0100

    Concierge: refactor frontend

--- concierge/concierge/__init__.py
+++ concierge/concierge/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask
 from flaskext.sqlalchemy import SQLAlchemy
 
 
@@ -11,25 +11,8 @@ except: pass
 db = SQLAlchemy(app)
 
 
+from concierge.frontend import frontend
+app.register_module(frontend)
+
 from concierge.auth import auth
 app.register_module(auth, url_prefix='/auth')
-
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-
-@app.route('/historico/')
-def historico():
-    return render_template('historico.html')
-
-
-@app.route('/login/')
-def login():
-    return render_template('login.html')
-
-
-@app.route('/service/')
-def service():
-    return render_template('service.html')
