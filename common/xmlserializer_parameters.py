14aeb897d2c9d243d356a2c6549b5a5f809da51d
goncalopp <goncalopp@gmail.com>
2011-04-21 11:40:23 +0100

    corrected identation on xmlserializer_parameters

--- common/xmlserializer_parameters.py
+++ common/xmlserializer_parameters.py
@@ -7,13 +7,13 @@ import sqlalchemy
 kinds= [ x for x in vars(xml_kinds).values() if inspect.isclass(x) and sqlalchemy.types.AbstractType in inspect.getmro(x)]
 
 def atribute_xml_filter(atr):
-	is_kind= atr.atr_class in kinds			#is a xml_kind
-	is_model_list= atr.is_model_list()	#is a list of models (relationship)
-	is_empty= (atr.atr_obj==None) or (is_model_list and len(atr.atr_obj)==0)	#value is null, or model list is empty
-	return not is_empty and (is_kind or is_model_list)
-	
+  is_kind= atr.atr_class in kinds        #is a xml_kind
+  is_model_list= atr.is_model_list()     #is a list of models (relationship)
+  is_empty= (atr.atr_obj==None) or (is_model_list and len(atr.atr_obj)==0)  #value is null, or model list is empty
+  return not is_empty and (is_kind or is_model_list)
+
 def atribute_xml_tagname(atr):
-	return "entity"
+  return "entity"
 
 def atribute_xml_atributes(atr):
   if atr.atr_class==None:
@@ -27,11 +27,11 @@ def atribute_xml_atributes(atr):
 
 
 def model_xml_filter(model):
-	return True
+  return True
 def model_xml_tagname(model):
-	return "entity"
+  return "entity"
 def model_xml_atributes(model):
-	return {'kind': model.model_class.__name__, 'type': LIST_TYPE}
+  return {'kind': model.model_class.__name__, 'type': LIST_TYPE}
 
 
 
