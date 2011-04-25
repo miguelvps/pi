12a476f500900c4d733c5a2035c674112d2dbe5d
goncalopp <goncalopp@gmail.com>
2011-04-25 16:21:25 +0100

    some bug fixing and development on search and service_metadata_parser method execute

--- concierge/concierge/search.py
+++ concierge/concierge/search.py
@@ -1,7 +1,6 @@
 from flask import Module, render_template, request, Response
 from concierge.services import Service
-from concierge.service_metadata_parser import ServiceMetadata
-
+from concierge.service_metadata_parser import ServiceMetadata, ServiceMetadataResourceMethod
 
 from flaskext.wtf import Form, Required
 from flaskext.wtf.html5 import SearchField
@@ -31,21 +30,23 @@ def match_search_to_methods_keywords(query, methods):
                 pass    #no match
     return queries_methods
     
-@search.route('/search/', methods=['GET','POST'])
+@search.route('/search/', methods=['POST'])
 def search_view():
-
     form = SearchForm(request.form)
     
     if form.validate_on_submit():
-        return render_template('search.html')
-        query= form.search_query
+        query= form.search_query.data
         
         services= Service.query.all()
         services_urls= [s.url for s in services]
         metadatas= map(ServiceMetadata, services_urls)
         search_methods= [m.global_search() for m in metadatas]
         
-        tmp= match_search_to_methods_keywords(query, search_methods)
-        #return Response(response= tmp)
-        return render_template('search.html')
-    return render_template('seds.html')
+        matches = match_search_to_methods_keywords(query, search_methods)
+        if len(matches)==0:
+            #no keywords match
+            results=[]
+        else:
+            results= [method.execute(query= query) for query, method in matches]
+        return render_template('search.html', search_results=results)
+    
