267b31b90316841a5c0f42fd2950e9f1a77e1517
Miguel Serrano <miguelvps@gmail.com>
2011-04-26 16:13:18 +0100

    Concierge: requirements

--- concierge/setup.py
+++ concierge/setup.py
@@ -5,8 +5,6 @@ setup(
     version='0.1',
     packages=find_packages(),
     install_requires=[
-        'SQLAlchemy',
-        'WTForms',
         'blinker',
         'Flask',
         'Flask-SQLAlchemy',
