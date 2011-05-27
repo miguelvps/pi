e831f6429fd0d04605de8ebf3f7dcf05865e6f2b
goncalopp <goncalopp@gmail.com>
2011-05-27 10:43:29 +0100

    pes: re-added xml_name to Person

--- services/pes/pes/app.py
+++ services/pes/pes/app.py
@@ -3,11 +3,9 @@ from flaskext.sqlalchemy import SQLAlchemy
 from sqlalchemy import or_
 from sqlalchemy.orm import joinedload
 
-from common import xml_attributes
-from common import xml_kinds
-from common import search
-from common import xser
+from common import xser, xml_attributes, xml_kinds, xml_names
 from common.xser_parameters import SERIALIZER_PARAMETERS, SER_TYPE_SHALLOW, SER_TYPE_SHALLOW_CHILDREN
+from common import search
 from common.xser_property import set_xser_prop
 
 import urllib
@@ -62,6 +60,8 @@ class Person(db.Model):
     search_atributes= ["name"]
 
 set_xser_prop(Person, xml_kinds.KIND_PROP_NAME, xml_kinds.person)
+set_xser_prop(Person, xml_names.NAME_PROP_NAME, xml_names.pes_person)
+
 
 @app.route("/")
 def search_method():
