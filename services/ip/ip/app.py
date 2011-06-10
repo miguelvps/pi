5d915fddb5403bb08aafec356f77160e6eaa2134
goncalopp <goncalopp@gmail.com>
2011-06-10 21:41:41 +0100

    IP: lots of stuff

--- services/ip/ip/app.py
+++ services/ip/ip/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, Response, request, url_for
+﻿from flask import Flask, Response, request, url_for
 from flaskext.sqlalchemy import SQLAlchemy
 
 from common import search
@@ -11,14 +11,22 @@ db = SQLAlchemy(app)
 
 class Deadline(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(256))
+    name = db.Column(db.String(2048))
     date = db.Column(db.Date)
     process_id = db.Column(db.Integer, db.ForeignKey('process.id'))
+
+    def to_xml_full(self):
+        return '<entity type="string">%s: %s</entity>'% (self.name, self.date.strftime("%D/%M/%Y"))
 
 
 class Requisite(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(256))
+    description = db.Column(db.String(256))
+    process_id = db.Column(db.Integer, db.ForeignKey('process.id'))
+
+class RequisiteDocument(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(256))
     process_id = db.Column(db.Integer, db.ForeignKey('process.id'))
 
 
@@ -28,6 +36,9 @@ class Document(db.Model):
     name = db.Column(db.String(256))
     url = db.Column(db.String(2083))
     process_id = db.Column(db.Integer, db.ForeignKey('process.id'))
+
+    def to_xml_full(self):
+        return '<entity name="%s" type="url">%s</entity>'% (self.name, self.url)
 
 
 class Process(db.Model):
@@ -39,14 +50,38 @@ class Process(db.Model):
 
     deadlines = db.relationship('Deadline', backref='process')
     requisites = db.relationship('Requisite', backref='process')
-    documents = db.relationship('Document', backref='document')
+    documents = db.relationship('Document', backref='process')
+    requisite_documents = db.relationship('RequisiteDocument', backref='process')
 
     def permalink(self):
-        return url_for('processes', id=self.id)
+        return url_for('process', id=self.id)
 
     def to_xml_shallow(self):
         return '<entity type="string" service="IP" url="%s">%s</entity>' % (self.permalink(), self.name)
 
+    def to_xml_full(self):
+        xml= u'<entity type="record">'
+        xml+= u'<entity type="string">%s</entity>' % self.name
+        xml+= u'<entity name="Página do Processo" type="url">%s</entity>' % self.url
+        if self.info: xml+= u'<entity type="string">%s</entity>' % self.info             #<-----TYPE URL NOT DEFINED!
+        if self.documents:
+            xml+= u'<entity name="Documentos Informativos" type="list">'
+            for doc in self.documents:
+                doc.to_xml_full()
+            xml+= u'</entity>'
+        if self.requisite_documents:
+            xml+= u'<entity name="Documentos Necessários" type="list">'
+            for rd in self.requisite_documents:
+                xml+= u'<entity type="string">%s</entity>' % (rd.description)
+            xml+= u'</entity>'
+        if self.requisites:
+            xml+= u'<entity name="Requisitos" type="list">'
+            for req in self.requisites:
+                xml+= u'<entity type="string">%s</entity>' % (req.description)
+            xml+= u'</entity>'
+        xml+='</entity>'
+        return xml
+        
 @app.route("/")
 def s():
     q = request.args.get('query', '')   #quoted query
@@ -68,4 +103,4 @@ def processes():
 @app.route("/process/<id>")
 def process(id):
     process = Process.query.get_or_404(id)
-    return Response(response=process.to_xml(), mimetype="application/xml")
+    return Response(response=process.to_xml_full(), mimetype="application/xml")
--- services/ip/manager.py
+++ services/ip/manager.py
@@ -1,6 +1,8 @@
+﻿
 from flaskext.script import Manager, Server
 
-from ip.app import app, db
+from ip.app import app, db, Deadline, Requisite, Document, Process, RequisiteDocument
+import datetime
 
 manager = Manager(app)
 
@@ -24,5 +26,24 @@ def resetdb():
     db.drop_all()
     db.create_all()
 
-if __name__ == "__main__":
+@manager.command
+def fixtures():
+    """Drops and creates the database."""
+    db.drop_all()
+    db.create_all()
+    
+    doc1= Document(name='Notas de acesso', url='http://www.fct.unl.pt/sites/default/files/Crit%C3%A9rios%20m%C3%ADnimos%20e%20F%C3%B3rmulas%20nota_%20candidatura_2011_12.pdf')
+    doc2= Document(name= 'Provas de ingresso', url='http://www.fct.unl.pt/sites/default/files/prov_%20ingr_%202011_12_oficial.pdf')
+    rd1= RequisiteDocument(description=u'Documento de identificação + 2 fotocópias (B.I., Cartão de Cidadão ou Passaporte)')
+    rd2= RequisiteDocument(description=u'Documento de identificação + 2 fotocópias (B.I., Cartão de Cidadão ou Passaporte)')
+    rd3= RequisiteDocument(description=u'2 (duas) fotografias actualizadas, tipo passe')
+    rd4= RequisiteDocument(description=u'Boletim individual de saúde c/ a vacina do tétano em dia (original + 1 fotocópia)')
+    d1= Deadline(name=u"Propina - 1.ª Prestação: 333,24€", date=datetime.date(2011, 10, 5))
+    d2= Deadline(name=u"Propina - 2.ª Prestação: 333,24€", date=datetime.date(2011, 12, 5))
+    d3= Deadline(name=u"Propina - 3.ª Prestação: 333,24€", date=datetime.date(2012, 03, 6))
+    p= Process(name='Matricula Licenciatura', deadlines=[d1,d2,d3], documents=[doc1,doc2],  requisite_documents=[rd1,rd2,rd3,rd4],  url='http://www.fct.unl.pt/candidato/1o-ciclo-licenciaturas')
+    db.session.add(p)
+    db.session.commit()
+
+if __name__=='__main__':
     manager.run()
