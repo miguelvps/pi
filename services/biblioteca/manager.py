f1b9396fbefe46b3a534ceae31deabe09c414c59
Miguel Serrano <miguelvps@gmail.com>
2011-06-11 17:11:35 +0100

    Biblioteca: set manager server port

--- services/biblioteca/manager.py
+++ services/biblioteca/manager.py
@@ -1,9 +1,10 @@
-from flaskext.script import Manager
+from flaskext.script import Manager, Server
 
 from biblioteca.app import app
 
 
 manager = Manager(app)
+manager.add_command("runserver", Server(port = 5002))
 
 
 if __name__ == "__main__":
