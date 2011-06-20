dc27ae3caa6832c208c5b3573b02b5edbd62648b
goncalopp <goncalopp@gmail.com>
2011-06-20 19:19:01 +0100

    2

--- concierge/concierge/xml_to_html_g7.py
+++ concierge/concierge/xml_to_html_g7.py
@@ -1,13 +1,30 @@
 from common.awesomexml import AwesomeXml
 from xml.etree import ElementTree
 
-def transform_text(xml):
+def get_name(xml):
     name= xml.get('label')
-    name = name if name!="null" else None
+    return name if name!="null" else None
+
+def transform_text(xml):
+    name= get_name(xml)
     if name:
         newxml= ElementTree.Element('entity', {"type":"string", "name":name}, text=xml.text)
     else:
         newxml= ElementTree.Element('entity', {"type":"string"}, text=xml.text)
+    return newxml
+
+def transform_list(xml):
+    name= get_name(xml)
+    #t= xml.get('type')
+    if name:
+        newxml= ElementTree.Element('entity', {"type":"list", "name":name}, text=xml.text)
+    else:
+        newxml= ElementTree.Element('entity', {"type":"list"}, text=xml.text)
+    for child in xml.getchildren():
+        assert child.tagname=='el'
+        name= get_name(child) or "<link>"
+        link= child.get('iref')
+        ElementTree.SubElement(newxml, 'entity', {"type":"string", "service":"", "url":link}, text=name)
     return newxml
 
 def transform_record(xml):
@@ -20,6 +37,10 @@ def transform_record(xml):
 def transform_element(xml):
     if xml.tagname=='record':
         return transform_record(xml)
+    if xml.tagname=='list':
+        return transform_list(xml)
+    if xml.tagname=='text':
+        return transform_text(xml)
     
     
 
