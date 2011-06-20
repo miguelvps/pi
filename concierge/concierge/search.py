f70bc5ed49f9a78a909a474d57e7466bba023cdb
goncalopp <goncalopp@gmail.com>
2011-06-20 20:49:04 +0100

    lots of bugs

--- concierge/concierge/search.py
+++ concierge/concierge/search.py
@@ -124,6 +124,6 @@ def search_aux(query, services=None, add_to_history=True):
             result_xml = result_xml.getchildren()[0]
         for e in result_xml:
             xml.append(e)
-    import pdb; pdb.set_trace()
+            
     html= xml_to_html.render(xml)
     return render_template('search.html', html=html)
