5efd89881d618b5b4fa3c844d9ed3c2ac80d9e71
Miguel Gomes <mp.gomes89@gmail.com>
2011-06-21 16:52:59 +0100

    service integration fix

--- concierge/concierge/xml_to_html_g7.py
+++ concierge/concierge/xml_to_html_g7.py
@@ -1,5 +1,6 @@
 from common.awesomexml import AwesomeXml
 from xml.etree import ElementTree
+from services_models import Service
 
 def get_name(xml):
     name= xml.get('label')
@@ -25,14 +26,18 @@ def transform_list(xml):
         newxml= ElementTree.Element('entity', {"type":"list"})
         newxml.text=xml.text
     for child in xml.getchildren():
-        ElementTree.append(transform_list_element(child))
+        newxml.append(transform_list_element(child))
     return newxml
 
 def transform_list_element(xml):
     assert xml.tag=='el'
     link= xml.get('iref')
     name= get_name(xml) or ("<link>" if link else "")
-    newxml= ElementTree.Element('entity', {"type":"string", "service":"", "url":link})
+    service = filter( lambda service: link.startswith(service.url) ,Service.query.all() )
+    assert len(service) == 1
+    service = service[0]
+    rel_link = link[len(service.url)-1:]
+    newxml= ElementTree.Element('entity', {"type":"string", "service":service.name, "url":rel_link})
     newxml.text= name
     return newxml
     
@@ -53,17 +58,18 @@ def transform_element(xml):
         return transform_text(xml)
     if xml.tag=='el':
         return transform_list_element(xml)
+    if xml.tag=='data': 
+        return transform_xml(xml)
+    raise Exception("element not recognized : " + xml.tag)
     
     
 
 def transform_xml(xml):
-    assert xml.tag==xml
-    l= xml.getchildren()
-    assert len(l)==1
-    data= l[0]
+    data = xml
     assert data.tag=='data'
     l= data.getchildren()
+    import pdb; pdb.set_trace()
     assert len(l)==1
     element= l[0]
-    return transform_element(xml)
+    return transform_element(element)
         
