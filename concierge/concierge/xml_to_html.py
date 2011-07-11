45adae8a7521dc721f704588a854b4c80dc31fa3
goncalopp <goncalopp@gmail.com>
2011-07-11 01:45:00 +0100

    concierge map: moved html to template

--- concierge/concierge/xml_to_html.py
+++ concierge/concierge/xml_to_html.py
@@ -1,5 +1,6 @@
 from concierge.services_models import Service
 from xml.etree import ElementTree
+from flask import render_template
 
 def render_string(xml):
     return xml.text
@@ -47,67 +48,13 @@ def render_map(xml):
     assert len(geowkt) == 1
     coords, wkt_type = parse_geowkt(geowkt[0].text)
     min_coords, max_coords = get_bounds(coords)
-    html ='''
-    <style>
-        .page-map, .ui-content, #map-canvas { width: 100%%; height: 100%%; padding: 0; }
-    </style>
-    <script type="text/javascript"> 
-        // When map page opens get location and display map
+    result= render_template('map.html', min_lat= min_coords[0], min_lng= min_coords[1],
+           max_lat= max_coords[0], max_lng= max_coords[1],    
+            draw_coords = ",".join(['new google.maps.LatLng(%f, %f)'% (x,y) for (x,y) in coords])
+            )
+    print result
+    return result
 
-            var min_latlng = new google.maps.LatLng(%(min_lat)s, %(min_lng)s);
-            var max_latlng = new google.maps.LatLng(%(max_lat)s, %(max_lng)s);
-
-            var bounds = new google.maps.LatLngBounds(min_latlng, max_latlng);
-
-            var myOptions = {
-                zoom: 12,
-                center: bounds.getCenter(),
-                mapTypeId: google.maps.MapTypeId.SATELLITE
-            };
-
-            var map = new google.maps.Map(document.getElementById("map-canvas"),myOptions);
-            google.maps.event.trigger(map,'resize');
-
-            map.fitBounds(bounds);
-
-            var polygon;
-
-            var myCoords = [%(draw_coords)s];
-
-            // Construct the polygon
-            // Note that we don't specify an array or arrays, but instead just
-            // a simple array of LatLngs in the paths property
-            polygon = new google.maps.Polygon({
-                paths: myCoords,
-                strokeColor: "#FF0000",
-                strokeOpacity: 0.8,
-                strokeWeight: 2,
-                fillColor: "#FF0000",
-                fillOpacity: 0.35
-           });
-           polygon.setMap(map);
-           var markerOptions = {
-                position: bounds.getCenter(),
-                map: map,
-                title: "title",
-           };
-           var marker = new google.maps.Marker(markerOptions);
-
-       $('.page-map').live('pageshow',function(){
-            google.maps.event.trigger(map, 'resize');
-            map.setOptions(myOptions); 
-            map.fitBounds(bounds);
-        });
-
-        </script>
-        <div id="map-canvas">
-        </div>
-    ''' % {'min_lat': min_coords[0], 'min_lng': min_coords[1],
-           'max_lat': max_coords[0], 'max_lng': max_coords[1],    
-            'draw_coords' : ",".join(['new google.maps.LatLng(%f, %f)'% (x,y) for (x,y) in coords]),
-            }
-
-    return html
 
 def add_service_id(xml):
     service= xml.get('service')
